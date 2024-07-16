90c446f5a8f0515cbccc4cb36ba5dc0339f6bb51
clarencewongww <clarencewong@Clarences-MacBook-Air.local>
2024-07-16 21:24:40 +0800

    edit about me

--- views/about_me.py
+++ views/about_me.py
@@ -32,7 +32,7 @@ st.subheader("Education", anchor=False)
 st.write(
     """
     - 2024: Master of Education @ Harvard University (4.00/4.00)
-    - 2023: Specialist Diploma in AI Solutions Development @ Temasek Polytechnic (4.00/4.00)
+    - 2023: Specialist Diploma in Artificial Intelligence Solutions Development @ Temasek Polytechnic (4.00/4.00)
     - 2021: Post-graduate Diploma in Education @ NIE (Credit)
     - 2019: Bachelor of Science with Honours in Chemistry @ NUS (Highest Distinction)
     """
