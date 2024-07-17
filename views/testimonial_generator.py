252d108f7c75f5407f1296413b1a36604a847321
clarencewongww <clarencewong@Clarences-MacBook-Air.local>
2024-07-17 19:50:18 +0800

    added download as docx feature

--- views/testimonial_generator.py
+++ views/testimonial_generator.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import anthropic
 import hmac
+import docx
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+import datetime
+import io
+import os
 
 # Check password
 def check_password():
@@ -34,14 +39,16 @@ if not check_password():
 # Define parameters
 MODEL = "claude-3-haiku-20240307"
 API_KEY = st.secrets["ANTHROPIC_API_KEY"]
-SYSTEM = """
+cur_time = datetime.datetime.now()
+cur_year = cur_time.year
+SYSTEM = f"""
 You are a teacher/educator that is writing testimonial for students that are leaving the school at secondary 4 or 5 in Singapore. 
 Make use of the inputs given to write a 5 paragraph testimonial for the student. Each paragraph should be 300 words. Total 1500 words. 
 Emphasize on the acheivements and examples but never make up examples that are not stated. If needed, you can give generic statements. 
 Do not have any other statement or words besides the testimonial, no header, no title. \n
 Examples of some testimonial: \n
 1. \n
-[Name] was a student of [School] from [Year] to [Year].
+[Name] was a student of [School] from {cur_year-3} to {cur_year}.
 
 [Name] is a well-mannered student who is continually striving to improve her academic performance. She consciously tries to upgrade herself and gives her best effort so as to realise her full potential. With her hard work, she obtained Top in Standard in both Secondary 1 and Secondary 3. She works in a determined fashion and she likes to challenge herself to do better each time. She has been awarded the Edusave Scholarship for Secondary School for three consecutive years. [Name] is proactive in seeking feedback and ways to improve. She is able to command the attention of her peers through her confidence and good public speaking skills.
 
@@ -53,7 +60,7 @@ Being friendly and helpful, [Name] was respected by her peers and tutors alike. 
 [Name] has indeed demonstrated the qualities needed for success in life. These qualities will stand her in good stead in her future endeavours.
 
 2. \n
-[Name] was a student of [School] from [Year] to [Year].
+[Name] was a student of [School] from {cur_year-3} to {cur_year}.
 
 [Name] displays a lively intelligence and has a practical approach to life. He is also a bright and resourceful student. A fast learner, he grasps concepts easily. He is not afraid to assert his opinions with great reasoning power and persuasive arguments. Where his studies are concerned, he sees setbacks as opportunities for him to improve and upgrade himself further. When in doubt, he is never afraid to consult his classmates as well as his teachers. This has allowed him to obtain Edusave Good Progress Award in Secondary 2 as well as Edusave Scholarship for Secondary School in Secondary 3.
 
@@ -63,7 +70,7 @@ Being friendly and helpful, [Name] was respected by her peers and tutors alike. 
 [Name] is a problem solver and his creativity together with his tenacity will augur well for him and he certainly can carve out a successful future for himself if he puts his mind into what he wants to do.
 
 3. \n
-[Name] was a student of [School] from [Year] to [Year].
+[Name] was a student of [School] from {cur_year-3} to {cur_year}.
 
 [Name] is a well-mannered and respectful person. He is also an active and diligent student who has the potential for greater achievements. He is someone who would rise to the occasion and overcome any challenges. [Name] is also a good team player. During group discussions, he never fails to participate actively and ensures that the group tasks are completed successfully. He also does not allow setbacks to deter him from striving to attain these goals. When given constructive feedback regarding his academic subjects, he accepts it positively and tries to put into practice what has been recommended to him. 
 
@@ -89,15 +96,20 @@ st.header("Type in the attributes")
 # Set columns for input
 col1, col2 = st.columns([2,2])
 col3, col4 = st.columns([2,2])
+col5, col6 = st.columns([2,2])
 # columns 
 with col1:
     stu_name = st.text_input('Enter the name of student:')
 with col2:
     stu_lead = st.text_input('Enter some comments of student\'s leadership:')
 with col3:
-    stu_cca = st.selectbox("Select CCA of student:", ("Badminton", "Scouts", "NCC", "Basketball", "Drama"), index = None, placeholder="Select CCA...")
+    stu_cca = st.selectbox("Select CCA of student:", ("Badminton", "Scouts", "NCC", "Basketball", "Drama", "Band"), index = None, placeholder="Select CCA...")
 with col4:
     stu_att = st.text_input("Enter some comments on student's learning attitude:")
+with col5:
+    stu_sch = st.selectbox("Select school:", ("Temasek Secondary School", "Montfort Secondary School", "Dunman High School"), index = None, placeholder="Select School...")
+with col6:
+    pass
 
 
 def generate_testimonial():
@@ -110,17 +122,48 @@ def generate_testimonial():
         max_tokens=4096,
         system = SYSTEM,
         messages=[
-            {"role": "user", "content": f"Student name is {stu_name}, with leadership in {stu_lead} and has learning attitude of {stu_att}. He/she is from the Co-curricula activity of {stu_cca}."}
+            {"role": "user", "content": f"Student name is {stu_name} from {stu_sch}, with leadership in {stu_lead} and has learning attitude of {stu_att}. He/she is from the Co-curricula activity of {stu_cca}."}
         ]
     ) as stream: 
         for text in stream.text_stream:
                 full_testimonial += text
                 testimonial_placeholder.markdown(full_testimonial)
+        
+    # -----CREATE DOCUMENT -----
+    # Create a document
+    doc = docx.Document()
+
+    # Add a paragraph to the document
+    p = doc.add_paragraph()
+
+    # Add some formatting to the paragraph
+    p.paragraph_format.line_spacing = 2
+    p.paragraph_format.space_after = 0
+    p.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
+
+    # Add a run to the paragraph
+    run = p.add_run(full_testimonial)
+
+    # Add some formatting to the run
+    #run.bold = True
+    #run.italic = True
+    run.font.name = 'Arial'
+    run.font.size = docx.shared.Pt(12)
+
+    # Save the document
+    doc.save(f"{stu_name}.docx")
+   
 
 if st.button("Generate Testimonial"):
-        # Check if all mandatory fields are filled
+    # Check if all mandatory fields are filled
     if not stu_name or not stu_att or not stu_cca or stu_lead is None:
         st.error("Please fill in all the mandatory fields.")
     else:
         generate_testimonial()
-   
+        with open(f'{stu_name}.docx', 'rb') as f:
+            st.download_button('Download Testimonial as Docx', f, file_name=f'{stu_name}.docx')
+        docx_files = [ f for f in os.listdir(os.curdir) if f.endswith(".docx") ]
+        for f in docx_files:
+            os.remove(os.path.join(os.curdir, f))
+
+
